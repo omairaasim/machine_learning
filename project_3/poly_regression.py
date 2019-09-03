fbb26d40287490355ded905a5e48683dff5271e2
Omair Aasim <omairaasim@gmail.com>
2019-09-03 23:53:01 +0530

    Add files via upload

---
+++ project_3/poly_regression.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: omairaasim
+"""
+
+# Step 1 - Load Data
+import pandas as pd
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Step 2 - Fitting Linear Regression
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,y)
+
+# Step 3 - Visualize Linear Regression Results
+import matplotlib.pyplot as plt
+
+plt.scatter(X,y, color="red")
+plt.plot(X, lin_reg.predict(X))
+plt.title("Linear Regression")
+plt.xlabel("Level")
+plt.ylabel("Salary")
+plt.show()
+
+# Step 4 Linear Regression prediction
+lin_reg.predict([[6.5]])
+
+# Step 5 - Convert X to polynomial format
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)
+
+
+# Step 6 - Passing X_poly to LinearRegression
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly,y)
+
+# Step 7 - Visualize Poly Regression Results
+plt.scatter(X,y, color="red")
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)))
+plt.title("Poly Regression - Degree 4")
+plt.xlabel("Level")
+plt.ylabel("Salary")
+plt.show()
+
+
+# Step 8 Polynomial Regression prediction
+lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
