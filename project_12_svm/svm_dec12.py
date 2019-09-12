d317dfbce0370dea47b9c4a4ceb016851875883a
Omair Aasim <omairaasim@gmail.com>
2019-09-12 20:04:25 +0530

    Add files via upload

---
+++ project_12_svm/svm_dec12.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 12 20:21:32 2018
+
+@author: omairaasim
+"""
+
+# Step 1 - Load Data
+import pandas as pd
+dataset = pd.read_csv("iphone_purchase_records.csv")
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 3].values
+
+# Step 2 - Convert Gender to number
+from sklearn.preprocessing import LabelEncoder
+labelEncoder_gender =  LabelEncoder()
+X[:,0] = labelEncoder_gender.fit_transform(X[:,0])
+
+# Optional - if you want to convert X to float data type
+import numpy as np
+X = np.vstack(X[:, :]).astype(np.float)
+
+
+# Step 3 - Split into Train and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Step 4 - Feature Scaling
+from sklearn.preprocessing import StandardScaler
+ss_X = StandardScaler()
+X_train = ss_X.fit_transform(X_train)
+X_test = ss_X.transform(X_test)
+
+# Step 5 - Fit SVC Classifier
+from sklearn.svm import SVC
+classifier = SVC(kernel = "linear", random_state=0)
+classifier.fit(X_train, y_train)
+
+# Step 6 - Predict
+y_pred = classifier.predict(X_test)
+
+
+# Step 7 - Confusion Matrix
+from sklearn import metrics
+cm = metrics.confusion_matrix(y_test, y_pred) 
+print(cm)
+accuracy = metrics.accuracy_score(y_test, y_pred) 
+print("Accuracy score:",accuracy)
+precision = metrics.precision_score(y_test, y_pred) 
+print("Precision score:",precision)
+recall = metrics.recall_score(y_test, y_pred) 
+print("Recall score:",recall)
